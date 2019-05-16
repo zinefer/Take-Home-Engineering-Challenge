2e7e489ab53edfe034f8949679f7eb0465e0f831
zinefer <zinefer@gmail.com>
2019-05-16 15:25:33 -0600

    Parse data CSVs into the database

--- Gemfile
+++ Gemfile
@@ -42,6 +42,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Progress bar helper for CLI reporting
+gem 'ruby-progressbar'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,41 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+# Pass a csv file and a block this function will call the block with every row of the csv
+# and report progress to the standard output
+def each_as_csv(file)
+  # Path to csv file
+  tripdata_csv_path = Rails.root.join('db', 'data', file)
+
+  # Number of lines in file
+  total_trips = File.foreach(tripdata_csv_path).count - 1
+
+  CSV.foreach(tripdata_csv_path, headers: true) do |row|
+    yield(row)
+
+    progress.increment
+  end
+end
+
+# Import taxi data
+each_as_csv('taxi+_zone_lookup.csv') do |row|
+  # Filter the incoming data
+  data = { id: row['LocationID'], name: row['Borough'], zone: row['Zone'], service_zone: row['service_zone'] }
+  # Insert it into the database
+  Borough.create!(data)
+end
+
+# Loop over all the trip data csv
+['fhv_tripdata_2018-01.csv', 'green_tripdata_2018-01.csv', 'yellow_tripdata_2018-01.csv'].each do |file|
+  # Pull the trip type off the begininng of the filename
+  trip_type = file.split('_').first
+
+  each_as_csv(file) do |row|
+    data = { pick_up_time: row['Pickup_DateTime'], drop_off_time: row['DropOff_datetime'],
+             pick_up_borough_id: row['PUlocationID'], drop_off_borough_id: row['DOlocationID'],
+             vehicle_type: trip_type }
+
+    Trip.create!(data)
+  end
+end
