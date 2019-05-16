34890bd6e0356c7cfe3dcebe6dba8f8217240c53
zinefer <zinefer@gmail.com>
2019-05-16 14:56:42 -0600

    Testfix: I removed this route

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class SearchControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get search_index_url
+    get root_url
     assert_response :success
   end
 end
